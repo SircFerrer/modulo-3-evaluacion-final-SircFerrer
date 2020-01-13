d0590b90e801f7b09d27af67c0f13eb45302395b
sircferrer <ferrercarballo@gmail.com>
2020-01-13 08:26:23 +0100

    add homepage:./ to package.json and create docs folder

---
+++ docs/precache-manifest.71d543797e1279947f9bd36071b1d0ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f0bd1f54425719c3cd76883c8a8575",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dde24ec368e0a8c80835",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9eb372cea7c45826e20d",
+    "url": "./static/js/2.1c9c4a48.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.1c9c4a48.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dde24ec368e0a8c80835",
+    "url": "./static/js/main.51e089bf.chunk.js"
+  },
+  {
+    "revision": "c776b87109dc4c8c209c",
+    "url": "./static/js/runtime-main.62e6bced.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
